7487aca16b9fb74cb9d64768f81948ce34e58acf
Hector Dominguez <hdominguez@talpor.com>
2015-11-29 17:16:39 -0430

    Removing two React Warnings:
    1- Any use of a keyed object should be wrapped in
    React.addons.createFragment(object) before being passed as a child.
    2- Child objects should have non-numeric keys so ordering is
    preserved. Check the render method of FriendList. See
    https://fb.me/react-warning-keys for more information.

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,30 +1,28 @@
 import React, { Component, PropTypes } from 'react';
-import mapValues from 'lodash/object/mapValues';
+import map from 'lodash/collection/map';
 
 import styles from './FriendList.css';
 import FriendListItem from './FriendListItem';
 
 export default class FriendList extends Component {
-  static propTypes = {
-    friends: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired
-  }
+    static propTypes = {
+        friends: PropTypes.object.isRequired,
+        actions: PropTypes.object.isRequired
+    }
 
-  render () {
-    return (
-      <ul className={styles.friendList}>
-        {
-          mapValues(this.props.friends, (friend) => {
-            return (<FriendListItem
-              key={friend.id}
-              id={friend.id}
-              name={friend.name}
-              starred={friend.starred}
-              {...this.props.actions} />);
-          })
-        }
-      </ul>
-    );
-  }
-
+    render () {
+        return (
+            <ul className={styles.friendList}>
+                {
+                    map(this.props.friends, friend =>
+                        <FriendListItem
+                        key={friend.id}
+                        id={friend.id}
+                        name={friend.name}
+                        starred={friend.starred}
+                        {...this.props.actions} />)
+                }
+            </ul>
+        );
+    }
 }
