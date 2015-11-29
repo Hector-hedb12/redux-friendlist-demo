081ea53cccd4216c89e152145448a94b39a09806
Hector Dominguez <hdominguez@talpor.com>
2015-11-29 18:31:24 -0430

    Testing material-ui lib adding a topbar

--- src/containers/FriendListApp.js
+++ src/containers/FriendListApp.js
@@ -6,6 +6,18 @@ import { connect } from 'react-redux';
 import * as FriendsActions from '../actions/FriendsActions';
 import { FriendList, AddFriendInput } from '../components';
 
+const RaisedButton = require('material-ui/lib/raised-button');
+const DropDownMenu = require('material-ui/lib/drop-down-menu');
+const Toolbar = require('material-ui/lib/toolbar/toolbar');
+const ToolbarGroup = require('material-ui/lib/toolbar/toolbar-group');
+const ToolbarSeparator = require('material-ui/lib/toolbar/toolbar-separator');
+
+let filterOptions = [
+    { payload: '1', text: 'All' },
+    { payload: '2', text: 'Started' },
+];
+
+
 @connect(state => ({
     friendlist: state.friendlist
 }))
@@ -20,10 +32,24 @@ export default class FriendListApp extends Component {
         const actions = bindActionCreators(FriendsActions, dispatch);
 
         return (
-                <div className={styles.friendListApp}>
-                <h1>The FriendList</h1>
-                <AddFriendInput addFriend={actions.addFriend} />
-                <FriendList friends={friendsById} actions={actions} />
+                <div>
+                   <Toolbar className={styles.toolBar}>
+                      <ToolbarGroup key={0} float="left">
+                         <DropDownMenu menuItems={filterOptions} />
+                      </ToolbarGroup>
+
+                      <ToolbarSeparator/>
+
+                      <ToolbarGroup key={1} float="right">
+                         <RaisedButton label="Delete all" primary={true} />
+                      </ToolbarGroup>
+                   </Toolbar>
+
+                   <div className={styles.friendListApp}>
+                      <h1>The FriendList</h1>
+                      <AddFriendInput addFriend={actions.addFriend} />
+                      <FriendList friends={friendsById} actions={actions} />
+                   </div>
                 </div>
         );
     }
