f7b77aa1f71bf546b5489f17e1e07d4600205e95
Hector Dominguez <hdominguez@talpor.com>
2015-11-30 08:32:41 -0430

    fix: SelectField/DropDownMenu not working #1011

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import App from './containers/App';
 import { render } from 'react-dom';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
 render(<App />, document.getElementById('root'));
